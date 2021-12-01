b691d1a94213fe119e4b0c501126693702eff43c
NikitaBhoir <bhoir.nikita.18ce5504@gmail.com>
2021-12-01 20:36:08 +0530

    DiscountApi Added

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)//add IHost obj, 1st para is the extension of Host obj, 2nd for ety counts
+        {
+            int retryFoAvailability = retry.Value;
+            using (var scope = host.Services.CreateScope())//create new scope to get the services
+            {//et required sevices from dependancy injection
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating postresql database.");
+                    //establish connection
+                    using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();//manually explicitly open the connection
+
+                    //create new command
+                    using var command = new NpgsqlCommand
+                    {
+                        Connection = connection
+                    };
+
+                    //seedin data using commandtext
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+                    command.ExecuteNonQuery();
+
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+                    command.ExecuteNonQuery();
+                    
+                    logger.LogInformation("Migrated postresql database.");
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the postresql database");
+                    if (retryFoAvailability < 50)
+                    {
+                        System.Threading.Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryFoAvailability);
+                    }
+                }
+            }
+
+            return host;
+        }
+
+     
+    }
+}
---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,18 @@
+﻿using Discount.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {//all discount related operations ae pefomed here
+        Task<Coupon> GetDiscount(string productName);//take the poduct name and retun the whole coupon info
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+
+    }
+}
