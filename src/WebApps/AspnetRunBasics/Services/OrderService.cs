36b175c5b440f8d3a4bdaf64b405d8a76aea4591
NikitaBhoir <bhoir.nikita.18ce5504@gmail.com>
2021-12-12 20:17:32 +0530

    Ui Added

---
+++ src/WebApps/AspnetRunBasics/Services/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class OrderService: IOrderService
+    {
+        private readonly HttpClient httpClient;
+        //this client now looks for orderservice which we have configured in startup
+        public OrderService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await httpClient.GetAsync($"/Order/{userName}");    //consume internal microservices
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}
